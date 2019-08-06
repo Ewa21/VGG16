88df880eb571d426c8640a21d49d65285bc648bd
Ewa Tusień <virtuosity2106@gmail.com>
2019-08-06 22:25:11 +0200

    Added profiling for GPU memory.

--- vgg.py
+++ vgg.py
@@ -4,6 +4,24 @@ import matplotlib.pyplot as plt
 import keras
 from keras.preprocessing.image import ImageDataGenerator, load_img
 
+## extra imports to set GPU options
+import tensorflow as tf
+from keras import backend as k
+ 
+###################################
+# TensorFlow wizardry
+config = tf.ConfigProto()
+ 
+# Don't pre-allocate memory; allocate as-needed
+config.gpu_options.allow_growth = True
+ 
+# Only allow a total of half the GPU memory to be allocated
+config.gpu_options.per_process_gpu_memory_fraction = 0.5
+ 
+# Create a session with the above options specified.
+k.tensorflow_backend.set_session(tf.Session(config=config))
+###################################
+
 train_dir = '/macierz/home/s165128/dishRecognizer/src/networks/data/training'
 validation_dir = '/macierz/home/s165128/dishRecognizer/src/networks/data/validation'
 image_size = 224
